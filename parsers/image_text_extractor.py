c7bfb04ee135fbe105302f889120ef463b9ae364
Mr.X <81518780+AlterEgo00001@users.noreply.github.com>
2024-10-08 01:29:14 +0300

    Update image_text_extractor.py

--- parsers/image_text_extractor.py
+++ parsers/image_text_extractor.py
@@ -1,14 +1,26 @@
 import cv2
 import pytesseract
 from pytesseract import Output
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 def extract_text_from_image(image):
-    return pytesseract.image_to_string(image, output_type=Output.DICT)
+    """Извлекает текст из изображения, используя pytesseract"""
+    try:
+        return pytesseract.image_to_string(image, output_type=Output.DICT)
+    except Exception as e:
+        logging.error(f"Error extracting text from image: {e}")
+        return {}
 
 def associate_text_with_images(frames):
+    """Ассоциирует текст с изображениями"""
     text_image_pairs = []
-    for frame in frames:
+    for i, frame in enumerate(frames):
         text = extract_text_from_image(frame)
-        if text:
-            text_image_pairs.append((frame, text))
+        if text.get('text'):
+            text_image_pairs.append((frame, text['text']))
+            logging.debug(f"Text extracted from frame {i}: {text['text'][:50]}...")
+        else:
+            logging.debug(f"No text found in frame {i}")
     return text_image_pairs
